63ae9fd3d1716aae445c460d5120ccb5e4bbd22f
rputh055 <rajeshputhsakayala7@gmail.com>
2020-07-05 17:51:39 -0400

    Basic Working Summary model

--- app/model.py
+++ app/model.py
@@ -8,7 +8,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sentence_transformers import SentenceTransformer
 from sklearn.cluster import KMeans
-
+import zipfile
+import io
+from flask import send_file
+from transformers import T5Tokenizer, T5ForConditionalGeneration, T5Config
+import torch
+import json
 
 
 class Clustering(object):
@@ -29,14 +34,17 @@ class Clustering(object):
                 writer.writerow(newrow)
         outfile.close()
 
-    def sampling(self, regex_file):
-        read_csv = csv.reader(regex_file, delimiter = ',')
-        tweet_list = []
-        for row in read_csv:
-            tweet = row[0]
-            tweet_list.append(tweet)
-        self.sampled_tweets = sample(tweet_list, 10)
-        return self.sampled_tweets
+    def sampling(self):
+        with open(self.path+'/processed.csv') as csv_file:
+
+            read_csv = csv.reader(csv_file, delimiter = ',')
+            tweet_list = []
+
+            for row in read_csv:
+                tweet = row[0]
+                tweet_list.append(tweet)
+            self.sampled_tweets = sample(tweet_list, 99)
+            return self.sampled_tweets
 
     def embeddings(self, list_of_tweets):
         model = SentenceTransformer('bert-base-nli-mean-tokens')
@@ -53,15 +61,45 @@ class Clustering(object):
             df.drop(columns = ['cluster'], inplace = True)
             df.to_csv(self.path+"/cluster"+str(i)+".txt")
 
+    def summaries(self, k):
+        for i in range(k):
+            with open(self.path +'/cluster'+str(i)+'.txt', 'r') as file:
+                data = file.read().replace('\n', '')
+
+                model = T5ForConditionalGeneration.from_pretrained('t5-small')
+                tokenizer = T5Tokenizer.from_pretrained('t5-small')
+                device = torch.device('cpu')
+
+                preprocess_text = data.strip().replace("\n","")
+
+                t5_prepared_Text = "summarize: "+preprocess_text
+                #print ("original text preprocessed: \n", preprocess_text)
+
+                tokenized_text = tokenizer.encode(t5_prepared_Text, return_tensors="pt").to(device)
+
+                # summmarize
+                summary_ids = model.generate(tokenized_text,
+                                                    num_beams=4,
+                                                    no_repeat_ngram_size=2,
+                                                    min_length=30,
+                                                    max_length=100,
+                                                    early_stopping=True)
+
+                output = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+
+                #print ("\n\nSummarized text: \n",output)
+                with open(self.path + '/summary'+str(i)+'.txt', 'w') as sum:
+                    sum.write(output)
+
+
 
     def zip_file(self, pattern, name):
-        if request.method == 'POST':
-            if os.path.isdir(self.path):      #making zip file for the obtained clusters
-                    tweets_file = io.BytesIO()
-                    with zipfile.ZipFile(tweets_file, 'w', zipfile.ZIP_DEFLATED) as my_zip:
-                        for root, dirs, files in os.walk(self.path):
-                            for file in files:
-                                if re.search(pattern, file):
-                                    my_zip.write(os.path.join(root, file))
-                    tweets_file.seek(0)
+        if os.path.isdir(self.path):      #making zip file for the obtained clusters
+            tweets_file = io.BytesIO()
+            with zipfile.ZipFile(tweets_file, 'w', zipfile.ZIP_DEFLATED) as my_zip:
+                for root, dirs, files in os.walk(self.path):
+                    for file in files:
+                        if re.search(pattern, file):
+                            my_zip.write(os.path.join(root, file))
+            tweets_file.seek(0)
         return send_file(tweets_file, mimetype='application/zip', as_attachment=True, attachment_filename=name+".zip")
--- app/views.py
+++ app/views.py
@@ -3,7 +3,10 @@ import os
 from flask import render_template, request, redirect, url_for, send_file, flash
 from werkzeug.utils import secure_filename
 import csv
+import io
+import zipfile
 from app.model import Clustering
+import re
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
@@ -14,6 +17,8 @@ ALLOWED_EXTENSIONS = set(['csv','xlsx', 'xls'])
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 cluster_obj = Clustering(UPLOAD_FOLDER)
+
+temp_k = []
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -35,17 +40,44 @@ def browse_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
+            cluster_obj.regex(filename)
+            sampled_file = cluster_obj.sampling()
+            embeds = cluster_obj.embeddings(sampled_file)
+            return "success"
     return render_template('upload.html')
 
-@app.route('/uploads/<filename>', methods=['GET', 'POST'])
-def uploaded_file(filename):
-    #cluster_obj = Clustering(UPLOAD_FOLDER)
-    cluster_obj.regex(filename)
-    sampled_file = cluster_obj.sampling(cluster_obj.path + 'processed.csv')
-    embeds = cluster_obj.embeddings(sampled_file)
-    return "success"
-
 @app.route('/run', methods = ['GET', 'POST'])
-cluster_obj.zip_file(r'cluster[0-9]+', clusters)
+def zip_cluster():
+    if request.method == 'POST':
+        k = int(request.form['k_value'])
+        temp_k.append(k)
+        cluster_obj.cluster(k)
+        #cluster_obj.zip_file("r'cluster[0-9]'", 'clusters')
+        if os.path.isdir(UPLOAD_FOLDER):      #making zip file for the obtained clusters
+            tweets_file = io.BytesIO()
+            with zipfile.ZipFile(tweets_file, 'w', zipfile.ZIP_DEFLATED) as my_zip:
+                for root, dirs, files in os.walk(UPLOAD_FOLDER):
+                    #for file in UPLOAD_FOLDER:
+
+                    for file in files:
+                        if re.search(r'cluster[0-9]+', file):
+                            my_zip.write(os.path.join(root, file))
+            tweets_file.seek(0)
+    return send_file(tweets_file, mimetype='application/zip', as_attachment=True, attachment_filename="cluster.zip")
+
+
+@app.route('/run/summary', methods = ['GET', 'POST'])
+def zip_summary():
+    if request.method == 'POST':
+
+        cluster_obj.summaries(temp_k[0])
+
+        if os.path.isdir(UPLOAD_FOLDER):      #making zip file for the obtained clusters
+            tweets_file = io.BytesIO()
+            with zipfile.ZipFile(tweets_file, 'w', zipfile.ZIP_DEFLATED) as my_zip:
+                for root, dirs, files in os.walk(UPLOAD_FOLDER):
+                    for file in files:
+                        if re.search(r'summary[0-9]+', file):
+                            my_zip.write(os.path.join(root, file))
+            tweets_file.seek(0)
+    return send_file(tweets_file, mimetype='application/zip', as_attachment=True, attachment_filename="summary.zip")
